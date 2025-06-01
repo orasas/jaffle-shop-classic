8fbf78fa58ef3d913433ca0dae76a196c3be5838
Sean <seanasaro1@gmail.com>
2025-06-01 16:00:35 -0700

    try new

---
+++ airflow_home/dags/dbt_ecom_dag_new.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'you',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2),
+}
+
+with DAG(
+    dag_id='dbt_ecom_pipeline',
+    default_args=default_args,
+    start_date=datetime(2025, 6, 1),
+    schedule_interval='@daily',
+    catchup=False
+) as dag:
+
+    run_dbt = BashOperator(
+        task_id='run_dbt_models',
+        bash_command=(
+            "/Users/sean/projects/my_ecom_ae_demo/.venv/bin/dbt "
+            "run --profiles-dir /Users/sean/.dbt "
+            "--project-dir /Users/sean/projects/my_ecom_ae_demo"
+        )
+    )
+
+    test_dbt = BashOperator(
+        task_id='test_dbt_models',
+        bash_command=(
+            "/Users/sean/projects/my_ecom_ae_demo/.venv/bin/dbt "
+            "test --profiles-dir /Users/sean/.dbt "
+            "--project-dir /Users/sean/projects/my_ecom_ae_demo"
+        )
+    )
+
+    run_dbt >> test_dbt
